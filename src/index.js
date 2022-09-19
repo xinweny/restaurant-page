d71a96bc981ed0958f3a39981acaf9b044a64151
Xin Wen Yap <yapxinwen1608@gmail.com>
2022-09-19 12:42:09 -0400

    Add basic header rendering

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
 import { renderHome } from './home.js';
+
+const content = document.getElementById('content');
+
+const renderHeader = () => {
+	const header = document.createElement('div');
+	header.id = 'header';
+
+	const title = document.createElement('h1');
+	title.textContent = 'Breath of the Wok';
+	header.appendChild(title);
+
+	const tabs = document.createElement('ul');
+	for (const tabText of ['Home', 'Menu', 'Content']) {
+		let tab = document.createElement('li');
+		tab.textContent = tabText;
+		tabs.appendChild(tab);
+	}
+	header.appendChild(tabs);
+
+	content.appendChild(header);
+}
+
+renderHeader();
